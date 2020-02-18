6b2f9ae2b99ad533a1eca80d72919790744db830
Shakhzodbek Yuldoshev <shakhzodbek.yuldoshev@gmail.com>
2020-02-18 17:59:08 +0500

    56. outputting  lists

--- src/App.js
+++ src/App.js
@@ -53,23 +53,14 @@ class app extends Component {
 
         let persons = null;
 
-        if(this.state.showPersons){
+        if (this.state.showPersons) {
             persons = (
                 <div>
-                    <Person
-                        name={this.state.persons[0].name}
-                        age={this.state.persons[0].age}
-                        click={this.switchNameHandler.bind(this, 'Max')}/>
-
-                    <Person
-                        name={this.state.persons[1].name}
-                        age={this.state.persons[1].age}
-                        changed={this.nameChangedHandler}
-                        click={this.switchNameHandler.bind(this, 'Max!!')}> My Hobbies: Racing</Person>
-
-                    <Person
-                        name={this.state.persons[2].name}
-                        age={this.state.persons[2].age}/>
+                    {this.state.persons.map(person => {
+                        return  <Person
+                            name={person.name}
+                            age={person.age} />
+                    })}
                 </div>
             )
         }
@@ -86,6 +77,7 @@ class app extends Component {
             </div>
         );
         /*return  React.createElement('div', {className: 'App'},React.createElement('h1', null, "Does this work now?"));*/
+
     }
 };
 
