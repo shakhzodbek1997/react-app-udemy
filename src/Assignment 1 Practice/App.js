1b367483d76eef761d6a060f1ebb91e7b0ea7e90
Shakhzodbek Yuldoshev <shakhzodbek.yuldoshev@gmail.com>
2020-02-18 13:13:53 +0500

    we completed first and second tasks

---
+++ src/Assignment 1 Practice/App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <ol>
+                    <li>Tasks here 1</li>
+                    <li>Tasks here 2</li>
+                    <li>Tasks here 3</li>
+                    <li>Tasks here 4</li>
+                    <li>Tasks here 5</li>
+                    <li>Tasks here 6</li>
+                    <li>Tasks here 7</li>
+                    <li>Tasks here 8</li>
+                    <li>Tasks here 9</li>
+                    <li>Tasks here 10</li>
+                </ol>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/Assignment 1 Practice/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const userOutput = () => {
+  return (
+      <div>
+          <p>Some random text</p>
+          <p>i hope I'll be overwritten!</p>
+      </div>
+  );
+};
+
+export default userOutput;
